54c2c7b52c86a16d76c45e74a531d8de1eedab51
VeselinTu <127752753+VeselinTu@users.noreply.github.com>
2023-04-30 19:39:32 +0300

    Add files via upload

---
+++ TrainApplication/Account.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainApplication
+{
+    public class Account
+    {
+        public string id;
+        public string username ;
+        public string password;
+
+        public Account(string id, string username, string password)
+        {
+            this.id = id;
+            this.username = username;
+            this.password = password;
+        }
+    }
+}
